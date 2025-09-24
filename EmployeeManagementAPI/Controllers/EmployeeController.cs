46f029762b0695d714869a2550770ebdfb0e4d08
Melpomeni Kallimorou <m.kallimorou@megasoft.gr>
2025-09-24 13:22:54 +0300

    initial

---
+++ EmployeeManagementAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,120 @@
+﻿using EmployeeManagementAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementAPI.Controllers
+
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    /// <summary>
+    /// Controller for managing employee-related operations.
+    /// </summary>
+    public class EmployeeController : ControllerBase
+    {
+        #region GET ALL EMPLOYEES
+        
+        [HttpGet]
+
+        public async Task <ActionResult<IEnumerable<Employee>>> Get()
+        {
+            if (EmployeeData.Employees == null || EmployeeData.Employees.Count == 0)
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+            return await Task.FromResult(Ok(EmployeeData.Employees));
+        }
+        #endregion
+
+        #region GET EMPLOYEE BY ID
+
+        /// <summary>
+        /// Gets an employee by their ID.
+        /// </summary>
+        /// <param name="id"> identifier for employee</param>
+        /// <returns> returns the employee with the specified ID. </returns>
+        [HttpGet(Name = "GetEmployeeById/{id}")]
+        public async Task<ActionResult<Employee>> GetEmployeeById([FromQuery]int id)
+        {
+            var employee = EmployeeData.Employees.FirstOrDefault(e => e.Id == id);
+            if (employee == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+            return await Task.FromResult(employee);
+        }
+        #endregion
+
+        #region  CREATE NEW EMPLOYEE
+        /// <summary>
+        /// Checks if id is already exists and creates new employee to Employee list with new unique ID.
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns> returns employee and 201 status code </returns>
+        [HttpPost(Name = "CreateEmployee")]
+        public async Task<ActionResult> CreateEmployee([FromBody] Employee newEmployee)
+        {
+            var result = EmployeeData.Employees.FirstOrDefault(e => e.Id == newEmployee.Id);
+            if (result != null)
+            {
+                return StatusCode(StatusCodes.Status409Conflict, "Employee with the same ID already exists.");
+            }
+            else
+            {
+                int newId = EmployeeData.Employees.Max(e => e.Id) + 1;
+                newEmployee.Id = newId;
+                EmployeeData.Employees.Add(newEmployee);
+
+                return await Task.FromResult(StatusCode(StatusCodes.Status201Created, newEmployee));
+            }
+            }
+        #endregion
+
+        #region UPDATE EMPLOYEE DETAILS
+        /// <summary>
+        /// Updates the details of an existing employee.
+        /// </summary>
+        /// <param name="employee">The employee object containing updated information by ID. The object must not be null and must include valid
+        /// employee details.</param>
+        /// <returns>An <see cref="ActionResult"/> indicating the result of the operation. Returns a 404 status code if the
+        /// employee is not found.</returns>
+        [HttpPut(Name = "UpdateEmployee")]
+        public async Task<ActionResult> UpdateEmployee([FromBody] Employee employee)
+        {
+            var result = EmployeeData.Employees.FirstOrDefault(e => e.Id == employee.Id);
+            if (employee == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+
+            }
+            else {
+                
+                return await Task.FromResult(StatusCode(StatusCodes.Status201Created));
+            }
+
+        }
+        #endregion
+
+        #region DELETE EMPLOYEE BY ID
+        /// <summary>
+        /// Deletes an employee by their ID.
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns> return status code 404 if employee not found and 204 if deleted</returns>
+        [HttpDelete(Name = "DeleteEmployee")]
+        public async Task<ActionResult> DeleteEmployee([FromQuery] int id)
+        {
+            var employee = EmployeeData.Employees.FirstOrDefault(e => e.Id == id);
+            if (employee == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+            else
+                {
+                EmployeeData.Employees.Remove(employee);
+                return await Task.FromResult(StatusCode(StatusCodes.Status204NoContent));
+            }
+        }
+        #endregion
+    }
+}
---
+++ EmployeeManagementAPI/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagementAPI.Models
+{
+    /// <summary>
+    /// Represents an employee entity with identifying and work-related details.
+    /// Salary must be a non-negative number and can include commas as thousand separators.
+    /// </summary>
+    public class Employee
+    {
+        /// <summary>
+        /// identifier
+        /// </summary>
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Position { get; set; }
+        public required string Department { get; set; }
+        [RegularExpression(@"^(((\d{1,3})(,\d{3})*)|(\d+))(.\d+)?$", ErrorMessage = "Error")]
+        [Range(0, int.MaxValue)]
+        public decimal Salary { get; set; }
+
+    }
+    /// <summary>
+    /// A predefined list of employees with example data.
+    /// </summary>
+    public static class EmployeeData
+    {
+        public static List<Employee> Employees = new List<Employee>
+        {
+            new Employee { Id = 1, Name = "John Doe", Position = "Software Engineer", Department = "IT", Salary = 60000 },
+            new Employee { Id = 2, Name = "Jane Smith", Position = "Project Manager", Department = "IT", Salary = 75000 },
+            new Employee { Id = 3, Name = "Sam Brown", Position = "HR Specialist", Department = "HR", Salary = 50000 }
+        };
+    }
+
+}
